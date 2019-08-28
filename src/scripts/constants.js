17d7be9ad323b0a76c180378e0cb8401b27fbb9b
igor-provotorov <garynya@yandex.ru>
2019-08-28 17:56:27 +0300

    fix: update api token

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,4 +1,4 @@
-const TOKEN = '708bc2aa051f901df1eb9520cf4e2ca0779d8560';
+const TOKEN = '0b73f0ee01671cd3114316ff474a34dd0d0dc04d';
 const PER_PAGE = 10;
 const BASE_SEARCH_URL = 'https://api.github.com/search/users?q=';
 
