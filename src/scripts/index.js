6105f90a4ff899d10d133cde0699997eb3a6a5b9
igor-provotorov <garynya@yandex.ru>
2019-08-30 18:11:17 +0300

    revert to #e5f0fe7 commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,9 @@
-import { fromEvent, of, from, combineLatest, Observable } from 'rxjs';
+import { fromEvent, of } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-import { debounceTime, distinctUntilChanged, map, switchMap, tap, catchError, combineAll } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map, switchMap, tap, catchError } from 'rxjs/operators';
 
 import { getSearchUrl } from './helpers/getSearchUrl';
+import { HALF_SECOND_DELAY } from './constants';
 import '../styles/materialize.min.css';
 import '../styles/style.css';
 
@@ -10,10 +11,18 @@ const searchBox = document.querySelector('input');
 const resultsList = document.querySelector('ul');
 
 const renderUsers = name => {
-    const node = document.createElement('li');
-    const textnode = document.createTextNode(name);
-    node.appendChild(textnode);
-    resultsList.appendChild(node);
+    const make = name => {
+        const node = document.createElement('li');
+        const textnode = document.createTextNode(name);
+        node.appendChild(textnode);
+        resultsList.appendChild(node);
+    };
+
+    if (Array.isArray(name)) {
+        name.forEach(userName => make(userName));
+    } else {
+        make(name);
+    }
 };
 
 const fetchUsers = query =>
@@ -25,13 +34,11 @@ const fetchUsers = query =>
                 return of({ error: true, message: `Error ${response.status}` });
             }
         }),
-        catchError(err => {
-            return of({ error: true, message: err.message });
-        })
+        catchError(err => of({ error: true, message: err.message }))
     );
 
 const inputObservable = fromEvent(searchBox, 'input').pipe(
-    debounceTime(500),
+    debounceTime(HALF_SECOND_DELAY),
     map(event => event.target.value),
     distinctUntilChanged(),
     tap(() => (resultsList.innerHTML = '')),
@@ -39,13 +46,15 @@ const inputObservable = fromEvent(searchBox, 'input').pipe(
 );
 
 inputObservable.subscribe(data => {
+    let searchValue = '';
+
     if (data.total_count === 0) {
-        renderUsers('There are no such users');
+        searchValue = 'There are no such users';
     } else if (!data.error) {
-        data.items.forEach(item => {
-            renderUsers(item.login);
-        });
+        searchValue = data.items.map(item => item.login);
     } else {
-        renderUsers('You must enter something to search');
+        searchValue = 'You must enter something to search';
     }
+
+    renderUsers(searchValue);
 });
