b1369e84a70200c270bf6e5a0ebbda96ef5ff473
igor-provotorov <garynya@yandex.ru>
2019-08-30 18:05:09 +0300

    revert#2

--- src/scripts/helpers/getSearchUrl.js
+++ src/scripts/helpers/getSearchUrl.js
@@ -1,4 +1,4 @@
-import { BASE_SEARCH_URL, TOKEN, PER_PAGE, MAX_REPOS_PER_PAGE } from '../constants';
+import { BASE_SEARCH_URL, TOKEN, PER_PAGE } from '../constants';
 
 const getSearchUrl = searchValue => {
     return `${BASE_SEARCH_URL}${searchValue}+in%3Alogin&access_token=${TOKEN}&per_page=${PER_PAGE}`;
