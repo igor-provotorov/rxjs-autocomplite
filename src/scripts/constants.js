304f32f426fee481bd0670810deffd1ed6cd1980
igor-provotorov <garynya@yandex.ru>
2019-09-02 16:15:24 +0300

    change API_TOKEN

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,4 +1,4 @@
-const TOKEN = '787f0a72229d13d7735f502e08fc8cbad9808373';
+const TOKEN = '721ada97b8f795a061af8e6da238a0d2e38d6657';
 const BASE_SEARCH_URL = 'https://api.github.com/';
 const HALF_SECOND_DELAY = 500;
 const PER_PAGE = 10;
